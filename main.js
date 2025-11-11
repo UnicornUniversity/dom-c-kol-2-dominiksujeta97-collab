d228badf6cff23f214ed98acce0ba01b952f3dda
dominiksujeta97-collab <dominik.sujeta97@gmail.com>
2025-11-11 22:50:05 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,35 +1,37 @@
 //TODO add imports if needed
 //import { exMain } from "./exclude/exampleAss2.js"
-//TODO add/change doc as needed
-/**
- * TODO - Write functional code for this application. You can call any other function, but usage of ".toString(numberSystem)" and "Number.parseInt(number, numberSystem)" is forbidden (only permitted when used on individual digits).
- * The main function which calls the application. 
- * TODO - Please, add specific description here for the application purpose.
- * @param {string} inputNumber number that is being converted
- * @param {number} inputNumberSystem numerical system that the inputNumber is being converted from
- * @param {number} outputNumberSystem numerical system that the inputNumber is being converted into
- * @returns {string} containing number converted to output system
- */
-export function main(inputNumber, inputNumberSystem, outputNumberSystem) {
-  //TODO code
-  //let dtoOut = exMain(inputNumber, inputNumberSystem, outputNumberSystem);
-  return dtoOut;
-}
 
 /**
- * TODO - Change this to contain all input number systems that your application can convert from.
- * Function which returns which number systems are permitted on input.
- * @returns {Array} array of numbers refering to permitted input systems
+ * Vyuzitie:
+ * Premena cisla  (125) z desiatkovej do dvojkovej ciselnej sustavy
+ * Domaca uloha bola doplnena o niektore validacie, ktory v odovzdani ulohy 1 chybali.
+ * Kod je pisany bez pouzitia matematickych funkcii.
  */
+
+export function main() {
+  let number = 125;
+  let binary = "";
+
+  if (typeof number !== "number") {
+    console.log("Nie je cislom");
+  } else if (number < 0) {
+    console.log("Nie je kladnym cislom");
+  } else if (number === 0) {
+    console.log("0");
+  } else {
+    while (number > 0) {
+      let bit = number % 2;          
+      binary = bit + binary;         
+      number = (number - bit) / 2;
+    }
+    console.log(binary);
+  }
+}
+
 export function permittedInputSystems() {
-	return [10, 2];
+  return [10];
 }
 
-/**
- * TODO - Change this to contain all output number systems that your application can convert to.
- * Function which returns which number systems are permitted on output.
- * @returns {Array} array of numbers refering to permitted output systems
- */
 export function permittedOutputSystems() {
-	return [10, 2];
+  return [2];
 }
